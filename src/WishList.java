978ab616c189fd31440ba611af7a4f9e90ee695a
ppaula5 <adroverseguipaula@gmail.com>
2025-09-26 14:47:43 +0200

    dia1

---
+++ src/WishList.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class WishList extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("WishList");
+    }
+
+    public void settings(){
+        size(800, 800);
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+
+}
